e68ca5196546592f5967bb5a70ff11dbb1a71163
Caio Vinicius <caiob7517@gmail.com>
2026-09-20 01:33:25 -0300

    feat: docker container prune -f

--- app/composables/useVueCharts/useDonuChart.ts
+++ app/composables/useVueCharts/useDonuChart.ts
@@ -23,7 +23,7 @@ export function useDonutChart(data: MaybeRefOrGetter<DonutDatum[]>) {
             {
                 type: 'pie',
                 radius: ['45%', '70%'],
-                center: ['55%', '45%'],
+                center: ['60%', '45%'],
                 avoidLabelOverlap: true,
                 itemStyle: {borderRadius: 10, borderColor: '#fff', borderWidth: 7},
                 label: { formatter: '{b}\n{d}%' },
